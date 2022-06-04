62d1a265a072700a8e8a8df944bf3657a7a13dc5
tristan-oks <98077677+tristan-oks@users.noreply.github.com>
2022-06-04 10:08:24 +0200

    first commit

---
+++ src/test/java/com/safetynetalerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynetalerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
